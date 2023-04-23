13b44eb0b1f7edbf3aa142c8e407e9691416de35
Dmitry <65163135+del0ss@users.noreply.github.com>
2023-04-23 16:20:01 +0300

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".form > button")
-if (btn) {
-    btn.onclick = e => {
-        localStorage.setItem('myCat', 'Tom')
-        console.log(localStorage.getItem('myCat'))
-    }
-}
