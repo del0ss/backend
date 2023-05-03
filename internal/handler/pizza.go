1fcb870306a0a8d775816432e7a27bd90aa0ba91
Dmitry Delols <dimonsokolov228@mail.ru>
2023-05-03 10:51:44 +0300

    fixed pizza func name

--- internal/handler/pizza.go
+++ internal/handler/pizza.go
@@ -16,7 +16,7 @@ func (h *Handler) getPizza() gin.HandlerFunc {
 		//	newErrorMessage(c, http.StatusUnauthorized, "invalid header")
 		//	return
 		//}
-		p, err := h.store.Pizza().GetPizzas(sort)
+		p, err := h.store.Pizza().GetPizza(sort)
 		if err != nil {
 			newErrorMessage(c, http.StatusInternalServerError, err.Error())
 			return
@@ -44,7 +44,7 @@ func (h *Handler) getCategories() gin.HandlerFunc {
 func (h *Handler) getPizzaById() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id, _ := strconv.Atoi(c.Param("id"))
-		p, err := h.store.Pizza().GetPizza(id)
+		p, err := h.store.Pizza().GetPizzaById(id)
 		if err != nil {
 			newErrorMessage(c, http.StatusInternalServerError, err.Error())
 			return
